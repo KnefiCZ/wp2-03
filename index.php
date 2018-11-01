3abd07ad7d6e561ac343b7d11b9c94ae59c93e33
Matejknejfl <matej.kneifl@seznam.cz>
2018-11-01 11:17:19 +0100

    CYKLUS FFFFFFFFFFFor:D:D:D JAKOŽE VTIP

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="cs" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>kok</title>
+  </head>
+  <body>
+
+  <?php
+/*
+      for ($index=0; $index < 100 ; $index++) {
+      echo $index + 1 . "<br>";
+    }
+*/
+
+/*
+    for ($i=100; $i > 0 ; $i=$i-1) {
+    echo $i-5 . "<br>";
+  }
+*/
+
+   for ($i=0; $i < 100; $i=$i+5) {
+    echo $i . "<br>";
+   }
+   
+    ?>
+  </body>
+  </html>
